41f85b9d1ffd93b1cf5131ceca16e53b26372a7e
Sanjana Sunil <sanjana99sunil@gmail.com>
2019-10-21 18:06:35 +0530

    added check for non hindi words

--- src/wiki_ner.py
+++ src/wiki_ner.py
@@ -9,6 +9,7 @@ class WikiParser:
 
     def __init__(self):
         wikipedia.set_lang("hi")
+        self.ners = []
 
 
     def get_content(self, title):
@@ -17,17 +18,18 @@ class WikiParser:
         
         try:
             page = wikipedia.page(title, auto_suggest=False)
-
-            # summary = page.summary.strip()
-            # summary = summary.replace('\r\n', '\n').replace('\r', '\n')
-            # summary = re.sub('\n+', "\n", summary).replace('\n', '\n\n')
-        
             return page.content
-
         except wikipedia.exceptions.PageError:
             return ""
         except wikipedia.exceptions.DisambiguationError:
             return ""
+
+
+    def is_hindi_word(self, word):
+        for char in word:
+            if not (ord(u'\u0900') <= ord(char) <= ord(u'\u097F')):
+                return False
+        return True
 
 
     def extract_ners(self, doc):
@@ -36,39 +38,43 @@ class WikiParser:
         f.write(content)
         f.close()
 
-        ners = []
         ners_sentence = []
 
         os.system('make -C hindi-part-of-speech-tagger tag')
         f = open('hindi-part-of-speech-tagger/hindi.output', 'r')
+
         line = f.readline()
-        prev_ner = False 
+        prev_tagged = False 
         while line:
+            tagged = False
             if '<s>' in line:
                 if len(ners_sentence) > 0:
-                    ners.append(ners_sentence)
+                    self.ners.append(ners_sentence)
                     ners_sentence = []
-                prev_ner = False
             else:
                 tags = line.split()
-                if len(tags) >= 2 and 'NNP' in tags and tags[0] != '==':
-                    if prev_ner:
-                        ners_sentence[-1] = ners_sentence[-1] + ' ' + tags[0]
-                    else:
-                        ners_sentence.append(tags[0])
-                    prev_ner = True
-                else:
-                    prev_ner = False
+                if len(tags) >= 2 and 'NNP' in tags:
+                    if self.is_hindi_word(tags[0]):         
+                        if prev_tagged:
+                            ners_sentence[-1] = ners_sentence[-1] + ' ' + tags[0]
+                        else:
+                            ners_sentence.append(tags[0])
+                        tagged = True
+            prev_tagged = tagged
             line = f.readline()
+
         if len(ners_sentence) > 0:
-            ners.append(ners_sentence)
+            self.ners.append(ners_sentence)
             ners_sentence = []
+        
         f.close()
-    
-        print(ners)
 
 
 
 if __name__ ==  "__main__":
     wiki_extract = WikiParser()
-    wiki_extract.extract_ners(wikipedia.random())
+
+    for i in range(10):
+        wiki_extract.extract_ners(wikipedia.random())
+    
+    print(wiki_extract.ners)
